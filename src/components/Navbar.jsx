cb70c484a9baf722b18713dab3ef8eff6a0487b2
Urooj Rafique <145989623+Uroojrafique5@users.noreply.github.com>
2025-07-25 03:43:53 -0700

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,15 +38,15 @@ const Navbar = ({ setSidebarOpen }) => {
     
           {/* Header */}
           <div className="flex items-center justify-between mb-8  lg:px-10 py-4">
-            <h1 className="text-2xl font-bold text-gray-800">Dashboard</h1>
+{/*             <h1 className="text-2xl font-bold text-gray-800">Dashboard</h1> */}
             <div className="flex items-center space-x-4">
               <div className="relative">
                 <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
-                <input
+{/*                 <input
                   type="text"
                   placeholder="Search here..."
                   className="pl-10 pr-4 py-2 border border-gray-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent max-sm:w-[40px] w-64"
-                />
+                /> */}
                 
               </div> 
               <div className="lg:hidden max-sm:block">
